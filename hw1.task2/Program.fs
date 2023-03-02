7a041967c72562772042912d84bcc2636736a2b0
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-03-03 01:11:05 +0300

    Add solution for hw.task2 and hw1.task3

---
+++ hw1.task2/Program.fs
@@ -0,0 +1,13 @@
+﻿let fibbonachi n =
+    match n with
+    | 0u -> None
+    | 1u -> Some 1I
+    | 2u -> Some 1I
+    | _ -> 
+        let rec loop iter fst snd =
+            match n - iter with
+            | 0u -> Some snd
+            | _ -> loop (iter + 1u) snd (fst + snd)
+        loop 2u 1I 1I
+        
+printf "%A" <| fibbonachi 100u
---
+++ hw1.task3/Program.fs
@@ -0,0 +1,8 @@
+﻿let reverse aList = 
+    let rec loop aList acc =
+        match aList with
+        | [] -> acc
+        | h :: t -> loop (List.tail aList) (h :: acc)
+    loop aList []
+
+printf "%A" <| reverse [1..5]
