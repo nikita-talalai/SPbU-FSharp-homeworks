1907070f2312671a75dd1c4f5074c451d882b029
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-03-03 01:17:15 +0300

    Add solution for hw1.task4

---
+++ hw1.task4/Program.fs
@@ -0,0 +1,10 @@
+﻿let func n m =                    // length of list: (n + m) - n + 1 = m + 1
+    if n < 0 || m <= -1 then []
+    else
+        let fst = pown 2 n
+        let rec loop fst iter acc =
+            if iter = m + 1 then []
+            else fst :: loop (fst * 2) (iter + 1) acc
+        loop fst 0 []
+    
+printf "%A" <| func 3 2
