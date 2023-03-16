33f9dd2e1c8201e5f486c6c5c524e6ee0a9ed38e
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-03-16 07:52:05 +0300

    Add solution for hw2.task1

---
+++ task1/Program.fs
@@ -0,0 +1,12 @@
+﻿let quantity_mod1 aList = aList |> List.map (fun x -> if x % 2 = 0 then 1 else 0 ) |> List.fold (+) 0
+let quantity_mod2 aList = aList |> List.filter (fun x -> x % 2 = 0) |> List.length
+let quantity_mod3 = List.filter (fun x -> x % 2 = 0) >> List.length
+
+open FsCheck
+let mod1_Is_mod2 (aList:list<int>) = quantity_mod1 aList = quantity_mod2 aList
+Check.Quick mod1_Is_mod2
+let mod1_Is_mod3 (aList:list<int>) = quantity_mod1 aList = quantity_mod3 aList
+Check.Quick mod1_Is_mod3
+let mod2_Is_mod3 (aList:list<int>) = quantity_mod2 aList = quantity_mod3 aList
+Check.Quick mod2_Is_mod3
+
