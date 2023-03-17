2dd3640ed77b58062df2da06ac1dffc8ef04eef4
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-03-17 12:14:48 +0300

    Add tests for hw2.task1

---
+++ hw2/task1/Tests.fs
@@ -0,0 +1,25 @@
+﻿module hw2.Tests
+    
+    open hw2.Program
+    open NUnit.Framework
+    open FsUnit
+    
+    [<Test>]
+    let ``Empty list`` () =
+        quantity_mod1 [1] |> should equal 0
+        
+    [<Test>] 
+    let ``Only even numbers`` () =
+        quantity_mod1 [for i in 1 .. 10 -> 2*i] |> should equal 10
+ 
+    [<Test>]    
+    let ``Only odd numbers`` () =
+        quantity_mod1 [1 .. 2 .. 5] |> should equal 0
+     
+    [<Test>]   
+    let ``Regular test`` () =
+        quantity_mod1 [1 .. 10] |> should equal 5
+    
+
+
+
