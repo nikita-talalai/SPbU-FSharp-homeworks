65e2a6a4f85c699bcf88fd98554af49608d05302
nikita-talalai <93539700+nikita-talalai@users.noreply.github.com>
2023-04-21 04:35:29 -0700

    Update hw1.task2/Program.fs
    Co-authored-by: Alex Berezhnykh <berejnih.alex2011@yandex.ru>

--- hw1.task2/Program.fs
+++ hw1.task2/Program.fs
@@ -1,8 +1,7 @@
 ﻿let fibonacchi n =                      // fibonacchi sequence: 1, 1, 2, 3, 5, 8, ...
     match n with                        //             indexes: 1, 2, 3, 4, 5, 6
     | 0u -> None
-    | 1u -> Some 1I
-    | 2u -> Some 1I
+    | 1u | 2u -> Some 1I
     | _ -> 
         let rec loop iter fst snd =
             match n - iter with
