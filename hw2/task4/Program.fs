bbafa1db6161eeeeec7836e104c66a5a9776678d
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-02 05:28:24 +0300

    Add solution for task4

---
+++ hw2/task4/Program.fs
@@ -0,0 +1,11 @@
+﻿module task4.Program
+let isPrime n =
+    match n with
+    | n when n <= 1 -> false
+    | n when n = 2 -> true
+    | n ->
+        2 :: [3 .. 2 .. (n |> float |> sqrt |> int) + 1]
+        |> List.filter (fun x -> n % x = 0)
+        |> List.isEmpty 
+let primeGenerator () =
+    (+) 2 |> Seq.initInfinite |> Seq.filter isPrime
