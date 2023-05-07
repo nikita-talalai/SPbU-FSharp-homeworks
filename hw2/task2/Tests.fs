9c7e73bbd2ecf108272187f2012b8254be3a9fa6
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-07 17:23:23 +0300

    Add solution and tests for task2 (Map for tree)
    Map realization for tree in CPS

---
+++ hw2/task2/Tests.fs
@@ -0,0 +1,16 @@
+﻿module task2.Tests
+
+open NUnit.Framework
+open FsUnit
+open task2.Program
+
+[<Test>]
+let ``Empty tree`` () =
+    mapTree id Empty |> should equal Empty
+    
+[<Test>]
+let ``Mapping not empty tree`` () =
+    let input = Node(1, Node(2, Empty, Empty), Node(3, Empty, Empty))
+    let expected = Node(2, Node(4, Empty, Empty), Node(6, Empty, Empty))
+    
+    ((*) 2, input) ||> mapTree |> should equal expected
