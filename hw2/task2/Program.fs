e1a1720989220fa48acb78ccfd7d5c127de99824
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-07 17:23:23 +0300

    Add solution and tests for task2 (Map for tree)
    Map realization for tree in CPS

---
+++ hw2/task2/Program.fs
@@ -0,0 +1,13 @@
+﻿module task2.Program
+
+type BinTree<'a> =
+    | Node of 'a * BinTree<'a> * BinTree<'a>
+    | Empty
+    
+let mapTree mapping tree =
+    let rec innerMap tree cont =
+        match tree with
+        | Node(x, l, r) ->
+            innerMap l (fun left -> innerMap r (fun right -> cont (Node(mapping x, left, right))))
+        | Empty -> cont Empty
+    innerMap tree id
