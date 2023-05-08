e72adf9b8b9b299c5b0efafc9e34d8f00e0ce520
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-08 23:29:38 +0300

    Add solution and tests for task3 (Parse tree)

---
+++ hw2/task3/Program.fs
@@ -0,0 +1,41 @@
+﻿module task3.Program
+
+type Expression =
+    | Const of int
+    | Plus of Expression * Expression
+    | Minus of Expression * Expression
+    | Times of Expression * Expression
+    | Divide of Expression * Expression
+
+let calculate exp =
+    let rec calcCPS exp cont =
+        match exp with 
+        | Const(x) -> cont x
+        | Plus(x, y) -> calcCPS x (fun x -> calcCPS y (fun y -> cont (x + y)))
+        | Minus(x, y) -> calcCPS x (fun x -> calcCPS y (fun y -> cont (x - y)))
+        | Times(x, y) -> calcCPS x (fun x -> calcCPS y (fun y -> cont (x * y)))
+        | Divide(x, y) ->
+            match calcCPS y Some with 
+            | Some 0 -> None 
+            | Some(y) -> calcCPS x (fun x -> cont (x / y))
+            | None -> None
+            
+    calcCPS exp Some 
+    
+    // calcCPS (2 + 3) id
+    // calcCPS 2 (fun x -> calcCPS 3 (fun y -> id (x + y)))
+    // (fun x -> calcCPS 3 (fun y -> id (x + y)) 2
+    // calcCPS 3 (fun y -> id (2 + y))
+    // (fun y -> id (2 + y)) 3
+    // id (2 + 3)
+    // Some 5
+    // calcCPS (2 / 0) id
+    // calcCPS 0 id
+    // (fun x -> calcCPS 3 (fun y -> id (x + y)) 0
+    // calcCPS 3 (fun y -> id (2 + y))
+    // (fun y -> id (2 + y)) 3
+    // id (2 + 3)
+    // 5 
+    
+let exp = Plus(Minus(Const(10), Const(3)), Times(Minus(Const(7), Const(1)), Const(4))) // (10 - 3) + (7 - 1) * 4
+printfn "%A" <| calculate exp
---
+++ hw2/task3/Tests.fs
@@ -0,0 +1,50 @@
+﻿module task3.Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open task3.Program
+
+[<Test>]
+let ``Addition works correct`` () =
+   Plus(Const(1), Const(1)) |> calculate |> should equal (Some 2)
+   
+[<Test>]
+let ``Subtraction works correct`` () =
+   Minus(Const(5), Const(2)) |> calculate |> should equal (Some 3)
+   
+[<Test>]
+let ``Multiplication works correct`` () =
+   Times(Const(2), Const(2)) |> calculate |> should equal (Some 4)
+   
+[<Test>]
+let ``Division works correct`` () =
+   Divide(Const(10), Const(2)) |> calculate |> should equal (Some 5)
+
+[<Test>]
+let ``Division by 0 should return None`` () =
+   Divide(Const(1), Const(0)) |> calculate |> should equal None
+   
+[<Test>]
+let ``Addition is commutative`` () =
+   let plustest l r = calculate(Plus(Const(l), Const(r))) = calculate(Plus(Const(r), Const(l)))
+
+   Check.QuickThrowOnFailure plustest
+
+[<Test>]
+let ``Multiplication is commutative`` () =
+   let timestest l r = calculate(Times(Const(l), Const(r))) = calculate(Times(Const(r), Const(l)))
+
+   Check.QuickThrowOnFailure timestest
+   
+[<Test>]
+let ``Addition is associative`` () =
+   let plustest a b c = calculate(Plus(Plus(Const(a), Const(b)), Const(c))) = calculate(Plus(Const(a), Plus(Const(b), Const(c))))
+   
+   Check.QuickThrowOnFailure plustest
+   
+[<Test>]
+let ``Multiplication is associative`` () =
+   let timestest a b c = calculate(Times(Times(Const(a), Const(b)), Const(c))) = calculate(Times(Const(a), Times(Const(b), Const(c))))
+
+   Check.QuickThrowOnFailure timestest
