5cff56cf66b546d4b6bbdef0241bb82cde916cdb
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-11 21:39:30 +0300

    Fix solutions

--- test/test/Program.fs
+++ test/test/Program.fs
@@ -1,23 +1,27 @@
-﻿// Task 1: Supermap
+﻿module test.Program
+// Task 1: Supermap
 
-let supermap aList func =
-    let rec rec_map aList func acc =
-        match aList with
+let supermap mapping list =
+    let rec map mapping list acc =
+        match list with
         | [] -> acc
-        | h :: t -> rec_map t func (func h :: acc)
-    rec_map aList func []
-    
-printfn "%A" <| supermap [1.0 ; 2.0 ; 3.0] (fun x -> [sin x ; cos x])
-
+        | h :: t -> map mapping t (List.append acc (mapping h))
+    map mapping list []
 
 // Task 2: Diamond
+
 let diamond n =
-    match n with
+    let calc_body = function
     | 0 -> []
-    | n -> [1 ..  2 .. (n * 2) - 1] @ [(n * 2) - 1 ..  -2 .. 1]
-let multiply text times = String.replicate times text
-let result n = List.map (fun n -> multiply "*"  n) (diamond n)
-printfn "%A" <| result 4
-
-
-
+    | n -> [1 ..  2 .. (n * 2) - 3] @ [(n * 2) - 1] @ [(n * 2) - 3 ..  -2 .. 1]
+    
+    let calc_indentations = function
+    | 0 -> []
+    | n -> [n - 1 .. -1 .. 1] @ [0] @ [1 .. n - 1]
+    
+    let multiply text times = String.replicate times text
+    
+    let body n = List.map (fun n -> multiply "*"  n) (calc_body n)
+    let indentations n = List.map (fun n -> multiply " " n) (calc_indentations n)
+    
+    List.iter2 (printfn "%s%s") (n |> indentations) (n |> body)
---
+++ test/test/Tests.fs
@@ -0,0 +1,13 @@
+﻿module test.Tests
+
+open NUnit.Framework
+open FsUnit
+open test.Program
+
+[<Test>]
+let ``Supermap with single output (like a usual map)`` () =
+    [1; 2; 3] |> supermap (fun x -> [(*) 2 x]) |> should equal [2; 4; 6]
+    
+[<Test>]
+let ``Supermap with several outputs`` () =
+    [1; 2; 3] |> supermap (fun x -> [ (*)2 x; (*)3 x]) |> should equal [2; 3; 4; 6; 6; 9]
