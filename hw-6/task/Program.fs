eeb406522bcf6bc5b22ec06de30f4f231a247501
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-05-23 13:15:22 +0300

    Add solution for hw-6

---
+++ hw-6/task/Program.fs
@@ -0,0 +1,17 @@
+﻿module task.Program
+open System
+
+type AddingStringsBuilder() =
+    member this.Bind(x:string, f) =
+        try 
+            x |> int |> f
+        with :? FormatException -> 
+            None
+    member this.Return(x) =
+        Some x
+        
+type RoundingBuilder(accuracy : int) =
+    member this.Bind(x:float, f) =
+        Math.Round(x, accuracy) |> f
+    member this.Return(x:float) =
+        Math.Round(x, accuracy)
