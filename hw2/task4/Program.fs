10038aee9f84d70d2cd9be0d2654e94363ae3ba9
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-07-24 22:49:03 +0300

    fix: remove potential risk out of memory exception

--- hw2/task4/Program.fs
+++ hw2/task4/Program.fs
@@ -1,11 +1,15 @@
 ﻿module task4.Program
-let isPrime n =
+let rec isPrime n =
     match n with
     | n when n <= 1 -> false
     | n when n = 2 -> true
+    | n when n % 2 = 0 -> false
     | n ->
-        2 :: [3 .. 2 .. (n |> float |> sqrt |> int) + 1]
-        |> List.filter (fun x -> n % x = 0)
-        |> List.isEmpty 
+         let rec  loop n m =
+            if m > (n |> float |> sqrt |> int) + 1 then true
+            elif n % m = 0 then false
+            else loop n (m + 2)
+         loop n 3
+         
 let primeGenerator () =
     (+) 2 |> Seq.initInfinite |> Seq.filter isPrime
