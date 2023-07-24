fad2eaa5e4255cd91f3e6538fbb0f1f9f5461fed
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-07-24 22:08:06 +0300

    test: remove redundant test cases

--- hw2/task4.tests/Tests.fs
+++ hw2/task4.tests/Tests.fs
@@ -3,19 +3,11 @@
 open task4.Program
 open NUnit.Framework
 open FsUnit
-open FsCheck
 
 [<Test>]
-let ``Negative numbers are not prime`` () =
-    seq{-10 .. -1} |> Seq.forall (isPrime >> not) |> should equal true
-
-[<Test>]
-let ``Edge cases are correct`` () =
-    (isPrime 0 && isPrime 1) |> should equal false
-
-[<Test>]
-let ``isPrime works correct for prime numbers`` () =
-    seq{2; 3; 5; 7; 11; 13; 17; 19; 23; 29} |> Seq.forall isPrime |> should equal true
+let ``isPrime works correct`` () =
+    seq{-2; -1; 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20; 23; 24; 25; 26; 27; 28; 29}
+    |> Seq.filter isPrime |> should equal <| seq{2; 3; 5; 7; 11; 13; 17; 19; 23; 29}
         
 [<Test>]
 let ``Expected generation`` () =
