02c104a7a71ae9eaab201a6ddbd7dff422f0ccd5
Nikita Talalai <nikita.talalai.2002@gmail.com>
2023-09-04 04:37:09 +0300

    test: add tests for lambda interpreter

---
+++ interpreter/interpreter.tests/Tests.fs
@@ -0,0 +1,28 @@
+﻿module interpreter.tests
+
+    open NUnit.Framework
+    open FsUnit
+    open lambda
+
+    [<Test>]
+    let ``Test normal strategy`` () =
+        let value =
+            Application(
+                        Abstraction("x", Var "y"),
+                        Application(
+                            Abstraction("x", Application(Var "x", Var "x")),
+                            Abstraction("x", Application(Var "x", Var "x"))))
+        
+        value |> betaReduction |> should equal (Var "y")
+
+    [<Test>]    
+    let ``Test on variable`` () =
+        let value = Var "x"
+            
+        value |> betaReduction |> should equal (Var "x")
+        
+    [<Test>]
+    let ``Test on lambda abstraction`` () =
+        let value = Abstraction("x", Var "y")
+
+        value |> betaReduction |> should equal (Abstraction("x", Var "y"))
